9b4d68a23fb92c78cfcbc9e57dca8a5f90ec6f45
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-09-19 10:51:56 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,15 @@
+
+
+print('===Olá usuário, vamos aos cálculos?===')
+numero_um = int(input('Digite um valor: '))
+numero_dois = int(input('Digite outro: '))
+
+soma = numero_um + numero_dois
+subtração = numero_um - numero_dois
+multiplicação = numero_um * numero_dois
+divisao = numero_um / numero_dois
+
+print('O resultado da soma é igual a: ', soma)
+print('O resultadp da subtração é igual a ', subtração)
+print('O o produto da multiplicação é igual a: ', multiplicação)
+print('O resultado da divisão é igual a: ', divisao)
